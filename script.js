30582ed587894f4abb74a8538f76e59afcdb87ae
L <89287469+eboy11398@users.noreply.github.com>
2021-09-01 14:48:47 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+
+function omni() {
+    var ima;
+    if (document.getElementById("omniman").src == "https://cdn.vox-cdn.com/thumbor/3y5Q5PZX9abr9p1JIXi8BrLoLkY=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/22482033/INVI_S1_FG_107_Still307.jpg") {
+    document.getElementById("omniman").src = "https://preview.redd.it/fg8u4w809p171.jpg?width=2400&format=pjpg&auto=webp&s=35f708ce06732c9699387a9ef1b5239c4f0ee253";
+    }
+    else {
+        document.getElementById("omniman").src = "https://cdn.vox-cdn.com/thumbor/3y5Q5PZX9abr9p1JIXi8BrLoLkY=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/22482033/INVI_S1_FG_107_Still307.jpg";
+    }
+}
+
+
